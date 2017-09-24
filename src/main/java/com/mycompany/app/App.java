ddbd6b71933ddeb95b3cbadea360a83908840c7f
Yunus Can Başeşme <ybasesme@etu.edu.tr>
2017-09-24 23:43:30 +0300

    Demo file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,3 @@
-package com.mycompany.app;
-
-import static spark.Spark.*;
-
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
 public class App
